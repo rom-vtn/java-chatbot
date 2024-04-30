94b1c42b84c7ef25c8c1b847438bece7a30b90b6
Romane <romane.viton@gmail.com>
2024-04-30 14:30:41 +0000

    initial commit

---
+++ src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/App.java
@@ -0,0 +1,13 @@
+package de.htwsaar.sose2024.ase.fourpeopleteam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/de/htwsaar/sose2024/ase/fourpeopleteam/AppTest.java
@@ -0,0 +1,20 @@
+package de.htwsaar.sose2024.ase.fourpeopleteam;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
