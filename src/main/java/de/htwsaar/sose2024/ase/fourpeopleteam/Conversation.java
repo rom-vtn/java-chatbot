671bbec49d2f1c5b325a7684c8b6ec989b95c3c0
rom-vtn <romahvito@gmail.com>
2024-06-04 17:41:23 +0200

    add JSON sending, encoding and decoding

--- src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/Conversation.java
+++ src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/Conversation.java
@@ -10,6 +10,19 @@ public class Conversation {
         messages = new ArrayList<>();
     }
 
+    public static Conversation makeStandardConversation() {
+        Conversation conv = new Conversation();
+        Message firstMessage = new Message("system", "You are a very helpful AI assistant. Please respond very concisely.");
+
+        conv.add(firstMessage);
+
+        return conv;
+    }
+
+    public void add(Message message) {
+        messages.add(message);
+    }
+
     public String toJson() {
         StringJoiner sj = new StringJoiner(",");
         for (Message m : messages) {
@@ -38,5 +51,48 @@ public class Conversation {
         public String toJson() {
             return "{\"role\":\"" + role + "\",\"content\":\"" + content + "\"}";
         }
+
+        public String getRole() {
+            return role;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == null) {
+                return false;
+            }
+            if (this.getClass() != other.getClass()) {
+                return false;
+            }
+            Message otherMessage = (Message) other;
+            return otherMessage.getRole().equals(role) &&
+                otherMessage.getContent().equals(content);
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Conversation otherConv = (Conversation) other;
+        if (messages.size() != otherConv.messages.size()) {
+            return false;
+        }
+        for (int i = 0; i < messages.size(); i++) {
+            Message ourMessage = messages.get(i);
+            Message theirMessage = otherConv.messages.get(i);
+            if (!(ourMessage.equals(theirMessage))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/RequestSender.java
+++ src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/RequestSender.java
@@ -1,7 +1,76 @@
 package de.htwsaar.sose2024.ase.fourpeopleteam;
 
-public class RequestSender {
-    private String endpoint;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
 
-    
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.http.HttpResponse;
+
+public class RequestSender {
+    private String baseUrl;
+    public static int MAX_ATTEMPTS = 3;
+
+    public Conversation.Message requestNextMessage(Conversation conversation) throws Exception {
+        String responseBody = null;
+        int i;
+        for (i = 0; i < MAX_ATTEMPTS; i++) {
+            try {
+                responseBody = attemptSendingMessage(conversation);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            } catch (IOException | InterruptedException e) {
+                //do nothing, try again
+            }
+        }
+
+        if (i == MAX_ATTEMPTS) {
+            throw new Exception("request limit exceeded");
+        }
+
+        Conversation.Message message = decodeResponse(responseBody);
+        return message;
+    }
+
+    private Conversation.Message decodeResponse(String response) {
+        JSONObject jsonResponse = new JSONObject(response);
+        JSONArray choices = jsonResponse.optJSONArray("choices");
+        if (choices == null) {
+            throw new JSONException("null choices");
+        }
+        JSONObject firstChoice = choices.getJSONObject(0);
+        if (firstChoice == null) {
+            throw new JSONException("null first choice");
+        }
+        JSONObject jsonMessage = firstChoice.getJSONObject("message");
+        if (jsonMessage == null) {
+            throw new JSONException("null json message");
+        }
+        String content = jsonMessage.getString("content");
+        if (content == null) {
+            throw new JSONException("null message content");
+        }
+
+        return Conversation.Message.makeAssistantMessage(content);
+    }
+
+    private String attemptSendingMessage(Conversation conversation) throws URISyntaxException, IOException, InterruptedException {
+        String bodyString = "{\"messages`\": " + conversation.toJson() + "}";
+        BodyPublisher bp = BodyPublishers.ofString(bodyString);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(new URI(baseUrl + "/v1/chat/completions")).POST(bp).build();
+
+        //NOTE: synchronous
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
 }
