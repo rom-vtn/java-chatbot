28d23ef7eaa7d700bb14ac88aec722b015347d2b
arold <aroldstephyl12@gmail.com>
2024-06-13 15:24:09 +0200

    add class Chatbot

---
+++ src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/Chatbot.java
@@ -0,0 +1,27 @@
+package de.htwsaar.sose2024.ase.fourpeopleteam;
+
+public class Chatbot{
+    private UserInterface userInterface;
+    private Conversation conversation;
+    private RequestSender requestSender;
+    public Chatbot(UserInterface userInterface, RequestSender requestSender){
+        this.userInterface= userInterface;
+        this.requestSender= requestSender;
+        this.conversation = new Conversation();
+    }
+
+    public boolean conversationRound(){
+        String input = userInterface.getInput();
+        Conversation.Message userMessage = Conversation.Message.makeUserMessage(input);
+        conversation.add(userMessage);
+        Conversation.Message response = null;
+        try {
+            response = requestSender.requestNextMessage(conversation);
+        } catch (Exception e) {
+            return false;
+        }
+        conversation.add(response);
+        userInterface.show(response.getContent());
+        return true;
+    }
+}
