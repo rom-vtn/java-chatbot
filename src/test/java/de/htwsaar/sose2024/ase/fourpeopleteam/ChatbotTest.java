f4875139a419e996118e311c68ce19a9f238ced9
rom-vtn <romahvito@gmail.com>
2024-07-04 21:49:00 +0200

    update versions with version plugin, add check check at test phase

---
+++ src/test/java/de/htwsaar/sose2024/ase/fourpeopleteam/ChatbotTest.java
@@ -0,0 +1,14 @@
+package de.htwsaar.sose2024.ase.fourpeopleteam;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/** Tests for the Chatbot class. */
+public class ChatbotTest {
+  /** TODO add more tests. */
+  @Test
+  public void sample() {
+    assertTrue(true);
+  }
+}
