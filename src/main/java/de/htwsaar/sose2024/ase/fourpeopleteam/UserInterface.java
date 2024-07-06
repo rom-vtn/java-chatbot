8ace416f6adcf8d14cc133c106ff2dedc56b3d05
arold <aroldstephyl12@gmail.com>
2024-07-06 12:51:18 +0200

    add Javadoc in CommandLineInterface.java and UserInterface.java

--- src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/UserInterface.java
+++ src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/UserInterface.java
@@ -1,9 +1,16 @@
 package de.htwsaar.sose2024.ase.fourpeopleteam;
 
-/** TODO docs. */
+/**
+ * It is an interface for defining a user interface that interacts with a chatbot application openAI.
+ * An implementation of this interface should provide the methods show(displays a specific content to the user)
+ * and getInput(retrieves input from the user)
+ */
 public interface UserInterface {
 
+
   void show(String content);
+
   
   String getInput();
+
 }
