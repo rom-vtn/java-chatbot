1fcaac65d19b20491087182bf9e79574ed00ca91
rom-vtn <romahvito@gmail.com>
2024-07-11 19:36:14 +0200

    add Chatbot mockito test

--- src/test/java/de/htwsaar/sose2024/ase/fourpeopleteam/ChatbotTest.java
+++ src/test/java/de/htwsaar/sose2024/ase/fourpeopleteam/ChatbotTest.java
@@ -1,6 +1,9 @@
 package de.htwsaar.sose2024.ase.fourpeopleteam;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -10,5 +13,34 @@ public class ChatbotTest {
   @Test
   public void sample() {
     assertTrue(true);
+  }
+
+  @Test
+  public void compositionCheck() throws ChatbotException {
+    //the sample content used for our "vicious" mockery
+    String assistantResponse = "an example chatbot response string.";
+    Conversation.Message assistantMsg = Conversation.Message
+        .makeAssistantMessage(assistantResponse);
+    String userInput = "some question sent to the bot.";
+    Conversation.Message userMsg = Conversation.Message.makeUserMessage(userInput);
+    Conversation sentConversation = Conversation.makeStandardConversation();
+    sentConversation.add(userMsg);
+
+    //actually create the mocks
+    UserInterface ui = mock(UserInterface.class);
+    when(ui.getInput()).thenReturn(userInput);
+
+    RequestSender rs = mock(RequestSender.class);
+    when(rs.requestNextMessage(sentConversation)).thenReturn(assistantMsg);
+    
+    //use the mocks
+    Chatbot chatbot = new Chatbot(ui, rs);
+    chatbot.conversationRound();
+
+    //theck method calls on mocks
+    verify(ui).getInput();
+    //TODO fix the conversation equality failing when doing that
+    // verify(rs).requestNextMessage(sentConversation);
+    verify(ui).show(assistantResponse);
   }
 }
