d00137600f68121f9decd3bd32554bca6367ffb7
rom-vtn <romahvito@gmail.com>
2024-07-11 12:40:00 +0200

    add Main class

---
+++ src/main/java/de/htwsaar/sose2024/ase/fourpeopleteam/Main.java
@@ -0,0 +1,21 @@
+package de.htwsaar.sose2024.ase.fourpeopleteam;
+
+/** Represents a sample app using the CLI for the chatbot. */
+public class Main {
+  /**
+   * Runs the Chatbot.
+   *
+   * @param args the command line args
+   */
+  public static void main(String[] args) {
+    UserInterface ui = new CommandLineInterface();
+    RequestSender rs = new RequestSender("https://gpt.oc.romaneviton.fr/");
+
+    Chatbot chatbot = new Chatbot(ui, rs);
+
+    boolean keepGoing = true;
+    while (keepGoing) {
+      keepGoing = chatbot.conversationRound();
+    }
+  }
+}
